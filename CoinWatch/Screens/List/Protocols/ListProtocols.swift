b3281f1f02861f32a3aa951de140daf77506126f
Anil <anil@meditopia.com>
2023-04-18 21:49:25 +0300

    Add list screen default components

---
+++ CoinWatch/Screens/List/Protocols/ListProtocols.swift
@@ -0,0 +1,14 @@
+//
+//  ListProtocols.swift
+//  CoinWatch
+//
+//  Created by Anıl Kul on 18.04.2023.
+//
+
+import Foundation
+
+protocol ListViewModelProtocol {
+    
+}
+
+
---
+++ CoinWatch/Screens/List/ViewModel/ListViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  ListViewModel.swift
+//  CoinWatch
+//
+//  Created by Anıl Kul on 18.04.2023.
+//
+
+import Foundation
